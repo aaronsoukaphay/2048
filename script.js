800aaedfab2d6c055756c4d87fa54f7f8a46e2c9
Aaron <aaronsoukaphay@gmail.com>
2023-06-09 19:17:08 -0700

    Add empty generateRandomTile function

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// use constant because board size does not change during our program
 const BOARD_SIZE = 4
 
 let board = [];
 
+// Initialize board to a multidimentional array with nested arrays representing rows
 function initializeBoard(){
     for(let i = 0; i < BOARD_SIZE; i++){
         let row = []
@@ -12,10 +14,16 @@ function initializeBoard(){
     }
 }
 
+// Custom printBoard function to increase readability of our board
 function printBoard(){
     board.forEach((row) => {
         console.log(row.join(" "))
     })
+}
+
+// function to generate a random tile on our board
+function generatedRandomTile() {
+
 }
 
 function main(){
