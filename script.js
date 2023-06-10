138de5bc4b1f20726d9c0d620577a87d46c424de
Aaron <aaronsoukaphay@gmail.com>
2023-06-09 19:37:27 -0700

    Added generateRandomTile implementation

--- script.js
+++ script.js
@@ -23,11 +23,35 @@ function printBoard(){
 
 // function to generate a random tile on our board
 function generatedRandomTile() {
+    // Create empty array 'emptyCells'
+    // Iterate throguh the board
+        // If board at the cell is 0
+            // Add cell coordinates to emptyCells
+    // We use Math.random() to choose a random index in the emptyCells array
+    // Using the random index, choose a random cell coordinate
+    // Insert a new tile into the empty cell
+    // Using the random cell coordinate, insert new title into our game board
 
+    let emptyCells = [];
+    for (let y = 0; y < BOARD_SIZE; y++){
+        for (let x = 0; x < BOARD_SIZE; x++){
+            if (board[y][x] === 0){
+                let coordinate = {
+                    x: x,
+                    y: y
+                };
+                emptyCells.push(coordinate)
+            }
+        }
+    }
+    let randomCellIndex = Math.floor(Math.random() * emptyCells.length)
+    let randomCellCoordinate = emptyCells[randomCellIndex];
+    board[randomCellCoordinate.y][randomCellCoordinate.x] = Math.random() < 0.9 ? 2 : 4;    
 }
 
 function main(){
     initializeBoard()
+    generatedRandomTile()
     printBoard()
 }
 
